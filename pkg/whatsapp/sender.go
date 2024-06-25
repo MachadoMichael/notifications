d1650083b85b1c6fbaedb9bcca38b58bdbe2998c
Michael <machado.m3@icloud.com>
2024-06-25 09:54:51 -0300

    refactor: setting whatsapp business configuration and removing twilio configs

--- pkg/whatsapp/sender.go
+++ pkg/whatsapp/sender.go
@@ -1,6 +1,35 @@
 package whatsapp
 
-func Sender(recipient, body string) error {
+import (
+	"encoding/json"
+
+	"github.com/MachadoMichael/notifications/infra/database"
+	"github.com/MachadoMichael/notifications/pkg/logger"
+	"github.com/MachadoMichael/notifications/schema"
+	"golang.org/x/exp/slog"
+)
+
+func Sender(enterpriseName, recipient, body string) error {
+
+	enterpriseData, err := database.EnterpriseRepo.ReadOne(enterpriseName)
+	if err != nil {
+		logger.ErrorLogger.Write(slog.LevelError, err.Error())
+	}
+
+	var enterprise schema.Enterprise
+	json.Unmarshal([]byte(enterpriseData), &enterprise)
+
+	apiUrl := "https://graph.facebook.com/v13.0/" + enterprise.PhoneNumberId + "/messages"
+	client := resty.New()
+
+	payload := map[string]interface{}{
+		"messaging_product": "whatsapp",
+		"to":                "recipient",
+		"type":              "text",
+		"text": map[string]string{
+			"body": body,
+		},
+	}
 	Params.SetTo(recipient)
 	Params.SetBody(body)
 
